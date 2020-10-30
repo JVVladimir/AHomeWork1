35d7f7da72d46fc1bf8ebf0df425312d945c207f
JVVladimir <vakashitsyn@yandex.ru>
2020-10-30 16:36:54 +0300

    add horizontal orientation

--- app/src/main/java/com/jvvladimir/ahomework1/MainActivity.kt
+++ app/src/main/java/com/jvvladimir/ahomework1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jvvladimir.ahomework1
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -9,25 +10,28 @@ import com.jvvladimir.ahomework1.model.NumericHolder
 
 class MainActivity : AppCompatActivity() {
 
-    var numericHolder = NumericHolder { it % 2 == 0 }
-
     companion object {
         const val SPAN_COUNT_VERTICAL = 3
+        const val SPAN_COUNT_HORIZONTAL = 4
     }
+
+    private var numericHolder = NumericHolder { it % 2 == 0 }
+    private var curSpanCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        curSpanCount =
+            if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) SPAN_COUNT_VERTICAL else SPAN_COUNT_HORIZONTAL
+
         val data = numericHolder.numerics
         val adapter = NumericAdapter(data)
         val recyclerView = findViewById<RecyclerView>(R.id.recycler).apply {
-            layoutManager = GridLayoutManager(context, SPAN_COUNT_VERTICAL)
+            layoutManager = GridLayoutManager(context, curSpanCount)
             this.adapter = adapter
         }
 
-        // TODO: maybe delete this row!
-        recyclerView.smoothScrollToPosition(adapter.itemCount - 1)
         findViewById<Button>(R.id.button).apply {
             setOnClickListener {
                 if (it == this) {
